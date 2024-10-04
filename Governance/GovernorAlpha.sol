cb11eede5715e4be6523746e463cb6b6b7eb5d58
deployer <deployer@example.com>
2024-10-04 11:23:36 +0800

    fix GEM-01

--- Governance/GovernorAlpha.sol
+++ Governance/GovernorAlpha.sol
@@ -6,10 +6,10 @@ contract GovernorAlpha {
     string public constant name = "Financeound Governor Alpha";
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of Finance
+    function quorumVotes() public pure returns (uint) { return 4000000e18; } // 4,000,000 = 4% of Finance
 
     /// @notice The number of votes required in order for a voter to become a proposer
-    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of Finance
+    function proposalThreshold() public pure returns (uint) { return 1000000e18; } // 1,000,000 = 1% of Finance
 
     /// @notice The maximum number of actions that can be included in a proposal
     function proposalMaxOperations() public pure returns (uint) { return 10; } // 10 actions
--- Governance/GovernorBravoDelegateG1.sol
+++ Governance/GovernorBravoDelegateG1.sol
@@ -27,7 +27,7 @@ contract GovernorBravoDelegate is GovernorBravoDelegateStorageV1, GovernorBravoE
     uint public constant MAX_VOTING_DELAY = 40320; // About 1 week
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    uint public constant quorumVotes = 400000e18; // 400,000 = 4% of Finance
+    uint public constant quorumVotes = 4000000e18; // 4,000,000 = 4% of Finance
 
     /// @notice The maximum number of actions that can be included in a proposal
     uint public constant proposalMaxOperations = 10; // 10 actions
